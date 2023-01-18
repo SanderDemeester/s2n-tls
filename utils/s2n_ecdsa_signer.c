c42e4e0e39c28bd982989e7e654577b16e019514
Sander Demeester <demeester.sander@gmail.com>
2023-01-17 21:51:12 -0800

    Adding s2n_ecdsa_signer files

---
+++ utils/s2n_ecdsa_signer.c
@@ -0,0 +1,5 @@
+#include "utils/s2n_ecdsa_signer.h"
+
+int sign(uint8_t *data, size_t length){
+    fprintf("%s %d \n", data, length);
+}
---
+++ utils/s2n_ecdsa_signer.h
@@ -0,0 +1,16 @@
+/*
+* Author: demees@ 
+* Summary: ECDSA using signer proxy
+*/
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+// #include <string.h>
+// #include <unistd.h>
+// #include <sys/types.h>
+// #include <sys/socket.h>
+// #include <netinet/in.h>
+// #include <netdb.h> 
+
+int sign(uint8_t *data, size_t length);
